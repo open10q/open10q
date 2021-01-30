05c85243c9fdf3f3881a40ce3ec46e3b27626fd6
adiludmer <adiludmer@gmail.com>
2021-01-30 19:00:32 +0200

    Add CI (#10)
    * Add CI
    
    * fix workflow
    
    * resolve tests dir
    
    * make UT verbose
    
    * fix import
    
    * test failure
    
    * fix test
    
    Co-authored-by: adi <adi@rookout.com>

--- src/sec_extractor/etl/etl_tests.py
+++ src/sec_extractor/etl/etl_tests.py
@@ -1,13 +1,14 @@
 import unittest
-
-from etl import CompaniesDB, load_companies
+import os
+from .etl import CompaniesDB, load_companies
 
 
 class TestETL(unittest.TestCase):
 
     def test_etl(self):
+        current_dir = os.path.dirname(__file__)
         db = CompaniesDB()
-        load_companies(db, "../data/test/sub.txt", "../data/test/num.txt")
+        load_companies(db, f"{current_dir}/../data/test/sub.txt", f"{current_dir}/../data/test/num.txt")
 
         apple = db.get_company_by_name("APPLE INC").toDict()
         self.assertTrue(len(apple) > 0)
